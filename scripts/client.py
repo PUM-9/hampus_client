d12fb1d201e374e5919dcb608ab6f990491fb5c5
Hampus Dunström <hampusdunstrom@gmail.com>
2017-02-16 11:47:43 +0100

    don't show my messages

--- scripts/client.py
+++ scripts/client.py
@@ -4,8 +4,9 @@ from chat_server.msg import Message
 
 
 def receive(data):
-    global archive
-    print "%s: %s" % (data.sender, data.message)
+    global name
+    if data.sender != name:
+        print "%s: %s" % (data.sender, data.message)
 
 
 pub = rospy.Publisher('chat_in', Message, queue_size=10)
